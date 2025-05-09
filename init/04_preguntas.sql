54b75c5eb66b5bb7951ff4d891716115ce3ddc64
Dilary18 <dilarycruz2015@gmail.com>
2025-05-09 12:16:11 -0600

    Creaci¢n modulo preguntas

---
+++ init/04_preguntas.sql
@@ -0,0 +1,23 @@
+CREATE TABLE quest (
+  id SERIAL PRIMARY KEY,
+  txt TEXT NOT NULL,
+  cat VARCHAR(100), 
+  lvl VARCHAR(50),  
+  lawid INT REFERENCES lawarticle(id) ON DELETE SET NULL
+);
+
+CREATE TABLE opt (
+  id SERIAL PRIMARY KEY,
+  qid INT REFERENCES quest(id) ON DELETE CASCADE,
+  txt TEXT NOT NULL,
+  correct BOOLEAN NOT NULL 
+);
+
+CREATE TABLE ans (
+  id SERIAL PRIMARY KEY,
+  uid INT REFERENCES "USER"(user_id),
+  qid INT REFERENCES quest(id),
+  optid INT REFERENCES opt(id),
+  correct BOOLEAN,
+  ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
